b7afae8a0bbb3707caea9abcca336e82aee4581a
Altrocore <shepelev.daniel.en@gmail.com>
2024-04-13 23:31:24 -0230

    Add integration test for CarController

---
+++ src/test/java/com/keyin/server/CarControllerTest.java
@@ -0,0 +1,43 @@
+package com.keyin.server;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CarControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @Test
+    @WithMockUser(username="admin", roles={"USER", "ADMIN"})
+    public void testSearchCarsEndpoint() throws Exception {
+        mockMvc.perform(get("/api/cars/search")
+                        .param("make", "Toyota")
+                        .param("model", "Camry")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].make").value("Toyota"))
+                .andExpect(jsonPath("$[0].model").value("Camry Hybrid"));
+    }
+}
+
